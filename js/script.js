97571f45f559173d2e3f1fb398faf62157a4e0c3
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-05-07 21:58:02 +0900

    Color

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const logoImg = document.querySelector(".main_title__logo");
+
+// function $(colorChange() {
+//     $(variable).css({
+//         "--color1": "#FEFF86",
+//         "--color2": "#B0DAFF",
+//         "--color3": "#B9E9FC",
+//         "--color4": "#DAF5FF"
+//     })
+//     console.log("change color!");
+// })
+
+logoImg.addEventListener("click", colorChange);
